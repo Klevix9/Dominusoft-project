cea6a9ff2392fb6e92f025839955ed2c2c12a0bb
X_0 <klevi.xhani9@gmail.com>
2024-04-22 16:52:22 +0200

    Started adding the animation for the nav button toggle

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ document.addEventListener('DOMContentLoaded', (e)=>{
         splash.classList.add('display-none');
     }, 0);//0 -> 2000 change later
 });
+
+
+//nav-bar button
+const nav_button = document.querySelector('.nav-button');
+const nav = document.querySelector('.navbar');
+
+let nav_toggle = false;
+
+function display_nav_bar(){
+    nav_toggle = !nav_toggle
+
+    if(nav_toggle){
+        nav.classList.remove('display-none');
+        console.log("toggled");
+
+    } else {
+        nav.classList.add('display-none');
+        console.log("not toggled");
+    }
+}
