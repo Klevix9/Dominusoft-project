476e1cf685cb90ba16b93b64f9acf4bf269be715
X_0 <klevi.xhani9@gmail.com>
2024-04-23 15:48:46 +0200

    -Made the navbar appear and dissapear on button click -Added the carousel #unfinished -Added a placeholder for the footer #unfinished

--- script.js
+++ script.js
@@ -8,21 +8,60 @@ document.addEventListener('DOMContentLoaded', (e)=>{
 });
 
 
+
+
 //nav-bar button
-const nav_button = document.querySelector('.nav-button');
-const nav = document.querySelector('.navbar');
+const nav_button = document.querySelector(".nav-button");
+nav_button.style.color = "black";
+const navbar = document.querySelector(".navbar");
 
 let nav_toggle = false;
-
-function display_nav_bar(){
+//on button click the navbar is displayed by changing the opacity
+nav_button.addEventListener('click', function() {
     nav_toggle = !nav_toggle
 
     if(nav_toggle){
-        nav.classList.remove('display-none');
-        console.log("toggled");
+        //change the opacity
+        navbar.style.opacity = 0.7;
+        //change the color of the button
+        nav_button.style.color = "white";
+        console.log("0");
 
     } else {
-        nav.classList.add('display-none');
-        console.log("not toggled");
+        navbar.style.opacity = 0;
+        nav_button.style.color = "black";
+        console.log("1");
     }
+});
+
+
+//Carousel
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
