9f358cee8d0462efe91528abf5755b7a47fea7e6
X_0 <klevi.xhani9@gmail.com>
2024-04-24 16:49:05 +0200

    -Finished the responsivity of the navbar -Added the main slideshow #not Finished

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var media_query = window.matchMedia("(min-width:992px)");
+
+
 //splash screen
 const splash = document.querySelector('#splashscreen');
 
@@ -8,60 +11,48 @@ document.addEventListener('DOMContentLoaded', (e)=>{
 });
 
 
-
-
-//nav-bar button
+// Get reference to the nav button element
 const nav_button = document.querySelector(".nav-button");
+// Set initial styles
 nav_button.style.color = "black";
 const navbar = document.querySelector(".navbar");
-
 let nav_toggle = false;
-//on button click the navbar is displayed by changing the opacity
-nav_button.addEventListener('click', function() {
-    nav_toggle = !nav_toggle
 
-    if(nav_toggle){
-        //change the opacity
-        navbar.style.opacity = 0.7;
-        //change the color of the button
-        nav_button.style.color = "white";
-        console.log("0");
-
-    } else {
-        navbar.style.opacity = 0;
-        nav_button.style.color = "black";
-        console.log("1");
-    }
+// Add event listener to the nav_button for click events
+nav_button.addEventListener("click", function() {
+  // Call toggle_nav function when the button is clicked
+  toggle_nav();
 });
+
+// Function to toggle the navbar visibility
+function toggle_nav() {
+  nav_toggle = !nav_toggle; // Toggle the value of nav_toggle
+
+  if (nav_toggle) {
+      // Change the opacity and color of the button when navbar is displayed
+      navbar.style.opacity = 0.7;
+      navbar.style.display = "block";
+      nav_button.style.color = "white";
+  } else {
+      // Hide the navbar and change the color of the button when navbar is hidden
+      navbar.style.opacity = 0;
+      navbar.style.display = "none"
+      nav_button.style.color = "black";
+  }
+}
 
 
 //Carousel
+let slideIndex = 0;
+showSlides();
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+function showSlides() {
+    let slides = document.getElementsByClassName("slides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 2.2 seconds
 }
